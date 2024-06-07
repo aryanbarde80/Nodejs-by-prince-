28e9445b6d3adc38d467695dcb30624eef56f394
Aryan Barde <147256003+aryanbarde80@users.noreply.github.com>
2024-06-07 20:40:25 +0530

    Add files via upload

---
+++ Day2 - Create server in Nodejs & Callback function in Javascript/node_tutorial/notes.js
@@ -0,0 +1,13 @@
+console.log('notes page is loaded');
+
+//to use below info export is needed
+var age = 25;
+const addNumber = function(a,b){
+    return a+b;
+}
+
+//We exported my making the object
+module.exports ={
+    age,
+    addNumber
+}
